0c9c78b5a58eac4cdfabf9ad40df587f426c898d
Angel Travieso <angeltraviesoc@gmail.com>
2022-10-27 11:28:46 -0400

    modificacion 1er ejercicio

--- JavaProgrammingExercises/src/exercises/Programming.java
+++ JavaProgrammingExercises/src/exercises/Programming.java
@@ -1,11 +1,27 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Programming {
+    
+    Scanner console = new Scanner(System.in);
     
     // 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
     public void program1() {
-        System.out.println(String.format("Hello \n%s", "Angel Travieso"));
+   
+        System.out.println("Escribe tu nombre por favor: ");
+        
+        // Escribir en consola
+        String name = console.nextLine();
+        
+        // Imprimir respuesta
+        System.out.println(String.format("Hello \n%s", name));
+        
     }
+    
+    
+    
+    
     
     
     
