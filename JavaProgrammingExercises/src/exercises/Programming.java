32b9fd56bf32ee77c68edd41803114af6ddc3d32
Angel Travieso <angeltraviesoc@gmail.com>
2022-10-27 11:22:48 -0400

    ejercicio 1

---
+++ JavaProgrammingExercises/src/exercises/Programming.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Programming {
+    
+    // 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
+    public void program1() {
+        System.out.println(String.format("Hello \n%s", "Angel Travieso"));
+    }
+    
+    
+    
+}
---
+++ JavaProgrammingExercises/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import exercises.Programming;
+
+public class Test {
+    
+    public static void main(String[] args) {
+           
+        Programming exercises = new Programming();        
+        
+        exercises.program1();
+        
+        
+    }
+    
+}
